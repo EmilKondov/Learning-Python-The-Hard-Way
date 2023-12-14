7ee5f88ae53a9e51be716c638443d5841d375a7c
Emil Kondov <122922427+EmilKondov@users.noreply.github.com>
2023-12-14 23:59:58 +0200

    Add files via upload

---
+++ ex11.py
@@ -0,0 +1,23 @@
+# print("How old are you?", end = " ")
+# age = input()
+# print("How tall are you?", end = " ")
+# height = input()
+# print("How much do you weight?", end = " ")
+# weight = input()
+#
+# print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+
+
+#
+# print("What type of cars do you like?", end = " ")
+# cars = input()
+# print("How much is your budget?", end = " ")
+# budget = int(input())
+# print("How old do you want the car to be?", end= " ")
+# car_age = int(input())
+#
+# print(f"What I understand is that you are looking for {cars} car, costing not more than {budget} leva and should not be older than {car_age} years.")
+
+print(input(), "What type of car you like?")
+print(input(), "What is your budget?")
+print(input(), "What time do you have?")
