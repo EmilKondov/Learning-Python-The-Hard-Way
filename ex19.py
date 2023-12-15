21fc2ce67e75bd34ddc2900a8ca2f9dfb2ed165a
Emil Kondov <122922427+EmilKondov@users.noreply.github.com>
2023-12-15 23:35:11 +0200

    Function exercises
    Here I practice the different way you can call a function and give the expected arguments.

---
+++ ex19.py
@@ -0,0 +1,26 @@
+def car_shop(brand, model, horsepower):
+    print(f"I like {brand} especially the {model} model which has {horsepower} horsepower")
+    print(f"If you have {horsepower} horsepower you can actually race anything")
+    print(f"Yes this {brand} is the best car ever made")
+
+
+    #One way
+#car_shop("bmw", "m3", 420)
+
+    #Second way
+# car = "bmw"
+# car_model = "m3"
+# car_horsepower = 420
+# car_shop(car, car_model, car_horsepower)
+
+    #Third way
+# car = input("Please enter you favorite car brand here: ")
+# car_model = input("Plese enter the model of this brand that you like the most:")
+# car_horsepower = input("Please enter the power of the current model: ")
+# car_shop(car, car_model, car_horsepower)
+
+    #Fourth way
+# car = input()
+# car_model = input()
+# car_horsepower = input()
+# car_shop(car, car_model, car_horsepower)
